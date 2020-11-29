e2fbbd66f164a5bed61f8b229ed9121f8ac77c71
Alexa Carrington <acarring@wellesley.edu>
2020-11-29 13:20:39 -0500

    Added functionality in js

---
+++ app.js
@@ -0,0 +1,39 @@
+//Selectors
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelectorAll('.player__slider');
+
+//Functions
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    };
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+//Event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
